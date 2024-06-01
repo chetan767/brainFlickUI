84f6f546735470aa723737e5188316a6944eed40
chetan767 <60475469+chetan767@users.noreply.github.com>
2024-06-02 04:20:18 +0530

    blue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar class="bg-green">
+      <q-toolbar class="bg-blue">
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
