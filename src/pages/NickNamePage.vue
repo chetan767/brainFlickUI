1c5d76aabb2747137cae962b24d9fec355cb1ccc
Arshdeep <arshdeepsinghbhatia3@gmail.com>
2024-06-27 02:43:52 +0530

    lottie added

--- src/pages/NickNamePage.vue
+++ src/pages/NickNamePage.vue
@@ -1,24 +1,35 @@
 <template>
   <q-page class="row justify-center items-center text-white">
-
-
     <div class="col-12 text-h6 text-weight-bold text-center">
 
-      <div class="row justify-center items-center">
-        <div class="col-12 text-white">
-          CHOOSE A COOL NICKNAME
-        </div>
-        <div class="col-11 col-sm-8 col-md-5 col-lg-3 q-mt-md myinput">
-          <q-input :input-style="{ fontSize: '18px' }" input-class="input-class text-white" outlined v-model="nickname"
-            autogrow class="secondary-border">
-            <template v-slot:append>
-            </template>
-          </q-input>
-        </div>
-        <div class="col-12 text-h6 text-weight-bold text-center q-mt-lg">
-          <q-btn size="md" rounded icon="play_circle_outline" label="Start Brain Flick" @click="startBrainFlick"
-            class="q-py-sm  q-px-lg btn-color" />
-        </div>
+      <div class="row">
+
+          <div class="col-6 text-white">
+            <div class="row justify-center ">
+              CHOOSE A COOL NICKNAME
+            </div>
+            <div class="row justify-center ">
+              <div class="col-4 q-mt-md myinput">
+                <q-input :input-style="{ fontSize: '18px' }" input-class="input-class text-white" outlined v-model="nickname"
+                  autogrow class="secondary-border">
+                  <template v-slot:append>
+                  </template>
+                </q-input>
+              </div>
+            </div>
+            <div class="row justify-center  q-mt-md">
+              <q-btn size="md" rounded icon="play_circle_outline" label="Start Brain Flick" @click="startBrainFlick"
+              class="q-py-sm  q-px-lg btn-color" />
+            </div>
+          </div>
+          <div class="col-6 text-left">
+            <div class="row ">
+              <Vue3Lottie ref="customControl" animationLink="https://lottie.host/a70624a9-d1a5-45b6-a462-2006689c8ec1/mFkdZdWR1W.json"
+                :height="200" :width="200" :loop="true" :speed="2" :autoPlay="true" :direction="forward"
+                :pauseAnimation="true" :pauseOnHover="true" />
+               
+            </div>
+          </div>
       </div>
     </div>
   </q-page>
@@ -30,6 +41,8 @@ import { clearGuestInfo, setNickname } from 'src/services/authService';
 import { useNicknameStore } from 'src/stores/nicknameStore';
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router';
+import { Vue3Lottie } from "vue3-lottie";
+
 const nicknameStore = useNicknameStore()
 const nickname = ref('')
 const router = useRouter()
